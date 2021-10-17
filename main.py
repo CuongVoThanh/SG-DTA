2b90326be1fa6ebf5e396e27ed6d9b807e345258
tanthanh0510 <trantanthanh0510@gmail.com>
2021-10-17 22:51:15 +0700

    update argument

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Stacked Graph Drug Target Interaction")
     parser.add_argument("mode", choices=["train", "test"], help="Train or test")
     parser.add_argument("--dataset", choices=["davis", "drugchem", "kiba"], help="Choose dataset", required=True)
-    parser.add_argument("--drug_embedding", choices=["gcn", "gat", "ginconv", "gat_gcn",'embedding'], help="Choose drug_embedding for drug", required=True)
-    parser.add_argument("--node_embedding", default="fc_layer", choices=["gcn", "gat", "ginconv", "gat_gcn", "fc_layer"], help="Choose embedding for drug-target network", required=True)
-    parser.add_argument("--protein_model", default="embedding", choices=["embedding","gcn",'embeddingdeep'], help="Choose embedding for protein target")
+    parser.add_argument("--drug_embedding", choices=["gcn", "gat", "ginconv", "gat_gcn",'embedding'], help="Choose drug_embedding for drug", required=True)    
+    parser.add_argument("--protein_embedding", default="embedding", choices=["embedding","gcn",'embeddingdeep'], help="Choose embedding for protein target")
     parser.add_argument("--model", default="graphdta", choices=["graphdta", "sgdta",'deepdta','dgraphdta'], help="Choose model", required=True)
+    parser.add_argument("--network_embedding", default="gcn", choices=["gcn", "gat", "ginconv", "gat_gcn"], help="Choose embedding for drug-target network")
     parser.add_argument("--type_data", default="dataDTA", choices=["dataDTA","dgraphDTA"], help="Choose model for dataset", required=True)
     
     parser.add_argument("--exp_name", help="Experiment name", required=True)    
